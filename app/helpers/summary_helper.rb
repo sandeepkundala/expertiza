7168e9ca9e38380495f64810102bb72884910d09
sandeepkundala <sandeepkundala@gmail.com>
2020-03-04 18:54:36 -0500

    reverting to old

--- app/helpers/summary_helper.rb
+++ app/helpers/summary_helper.rb
@@ -17,6 +17,7 @@ module SummaryHelper
         self.summary[round.to_s] = {}
         self.avg_scores_by_criterion[round.to_s] = {}
         self.avg_scores_by_round[round.to_s] = 0.0
+        included_question_counter = 0
 
         questions[round].each do |q|
           next if q.type.eql?("SectionHeader")
@@ -267,6 +268,8 @@ module SummaryHelper
       sum_scores.round(2)
     end
   end
+
+  module_function
 end
 
 # end required by autosummary
